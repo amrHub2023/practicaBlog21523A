dfcfa04646baa62e05ea2c44ff334dfecb470029
AleMoreno <amrmoreno07@gmail.com>
2023-09-25 23:54:24 -0300

    Ejercicio Blog con cambios

--- database.js
+++ database.js
@@ -1,10 +1,14 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
 // Instancia de conexión a la base de datos
-const sequelize = new Sequelize('baseblogbd', 'root', '', {
-    host: 'localhost',
+const sequelize = new Sequelize(
+    process.env. DB_NAME,
+    process.env. DB_USER,
+    process.env. DB_PASS,
+    { 
+    host:process.env. DB_HOST,
     dialect: 'mysql'
-});
+    });
 
 
 module.exports = {
